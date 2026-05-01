0eab7877cd3e4c4736c6426f1ec3140a9c2bfe88
havetobecyberresilientmate <dadhibalde19@gmail.com>
2026-05-01 23:04:55 +0200

    Add Arret class for managing bus stop passengers

--- Arret.cs
+++ Arret.cs
@@ -1,1 +1,37 @@
+using System;
 
+namespace MetrovilleTransport
+{
+    public class Arret
+    {
+        private string nom;
+        private int passagersEnAttente;
+
+        public string Nom { get { return nom; } }
+
+        public Arret(string nom)
+        {
+            this.nom = nom;
+            this.passagersEnAttente = 0;
+        }
+
+        public void AjouterPassagers(int nb)
+        {
+            if (nb < 0)
+                throw new ArgumentException("Le nombre de passagers ne peut pas être négatif.");
+            passagersEnAttente += nb;
+        }
+
+        public int FaireMonter(int nb)
+        {
+            int montent = Math.Min(nb, passagersEnAttente);
+            passagersEnAttente -= montent;
+            return montent;
+        }
+
+        public int GetPassagersEnAttente()
+        {
+            return passagersEnAttente;
+        }
+    }
+}
