62d7dc71ac193653a390666e68fbb1a97ae92bba
havetobecyberresilientmate <dadhibalde19@gmail.com>
2026-05-01 23:06:55 +0200

    Implement MetroAutomatique class with incident signaling

--- MetroAutomatique.cs
+++ MetroAutomatique.cs
@@ -1,1 +1,30 @@
+using System;
 
+namespace MetrovilleTransport
+{
+    public class MetroAutomatique : Vehicule
+    {
+        private bool modeAutonome;
+
+        public MetroAutomatique(int numero, int capacite, int puissanceMoteur)
+            : base(numero, capacite, puissanceMoteur)
+        {
+            this.modeAutonome = true;
+        }
+
+        public override void Demarrer()
+        {
+            Console.WriteLine($"Metro {Numero} - activation du pilote automatique");
+        }
+
+        public override string GetTypeVehicule()
+        {
+            return "Metro Automatique";
+        }
+
+        public void SignalerIncident(string description)
+        {
+            Console.WriteLine($"[INCIDENT Metro {Numero}] : {description}");
+        }
+    }
+}
