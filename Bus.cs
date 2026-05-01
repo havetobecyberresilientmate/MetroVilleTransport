6b1186ff7be2264266b00810c08153d1eb15fbd7
havetobecyberresilientmate <dadhibalde19@gmail.com>
2026-05-01 23:05:23 +0200

    Implement Bus class with localization features

--- Bus.cs
+++ Bus.cs
@@ -1,1 +1,40 @@
+using System;
 
+namespace MetrovilleTransport
+{
+    public class Bus : Vehicule, ILocalisable
+    {
+        private bool estEnZonePayante;
+        private double latitude;
+        private double longitude;
+
+        public double Latitude { get { return latitude; } }
+        public double Longitude { get { return longitude; } }
+
+        public Bus(int numero, int capacite, int puissanceMoteur, bool estEnZonePayante)
+            : base(numero, capacite, puissanceMoteur)
+        {
+            this.estEnZonePayante = estEnZonePayante;
+            this.latitude = 0;
+            this.longitude = 0;
+        }
+
+        public override void Demarrer()
+        {
+            Moteur.Demarrer();
+            string zone = estEnZonePayante ? "payante" : "gratuite";
+            Console.WriteLine($"Bus {Numero} prêt - zone {zone}");
+        }
+
+        public override string GetTypeVehicule()
+        {
+            return "Bus";
+        }
+
+        public void ActualiserPosition(double lat, double lon)
+        {
+            latitude = lat;
+            longitude = lon;
+        }
+    }
+}
