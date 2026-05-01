fa8611f4c37bdb5084fe99fbb2980028a52fbc1b
havetobecyberresilientmate <dadhibalde19@gmail.com>
2026-05-01 23:02:17 +0200

    Update ILocalisable.cs

--- ILocalisable.cs
+++ ILocalisable.cs
@@ -1,1 +1,9 @@
-
+namespace MetrovilleTransport
+{
+    public interface ILocalisable
+    {
+        double Latitude { get; }
+        double Longitude { get; }
+        void ActualiserPosition(double lat, double lon);
+    }
+}
